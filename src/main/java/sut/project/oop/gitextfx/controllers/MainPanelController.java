dd395883c99610029897ad10215a9b5a333719ed
LadsForTea <88262542+Absurdnoobman@users.noreply.github.com>
2025-12-30 19:47:29 +0700

    add menu bar functionality.

--- src/main/java/sut/project/oop/gitextfx/controllers/MainPanelController.java
+++ src/main/java/sut/project/oop/gitextfx/controllers/MainPanelController.java
@@ -4,7 +4,10 @@ import com.github.difflib.DiffUtils;
 import com.github.difflib.UnifiedDiffUtils;
 import com.github.difflib.patch.Patch;
 import com.github.difflib.patch.PatchFailedException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -12,18 +15,21 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import sut.project.oop.gitextfx.AppDateFormat;
 import sut.project.oop.gitextfx.AppPath;
+import sut.project.oop.gitextfx.GitextApp;
 import sut.project.oop.gitextfx.clazz.*;
 import sut.project.oop.gitextfx.models.Version;
 import sut.project.oop.gitextfx.models.VersionTag;
 
 import javax.swing.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
+import java.util.zip.GZIPOutputStream;
 
 public class MainPanelController {
     private Stage stage;
@@ -323,4 +329,94 @@ public class MainPanelController {
         alert.showAndWait();
     }
 
+    @FXML
+    private void onAddFilePressed(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text file", "*.txt"));
+        File result = fileChooser.showOpenDialog(null);
+
+        if (result == null) return;
+
+        String full_path = result.getPath();
+        long id = -1;
+        try (var db = new Schema()) {
+            id = db.insertAndReturnID("""
+                    INSERT INTO Files (file_path) VALUES (?)
+                    """, full_path);
+
+            if (id == -1) {
+                ErrorDialog.showException("Can not insert a file.");
+                return;
+            }
+
+            var bos = new ByteArrayOutputStream();
+            var gzip = new GZIPOutputStream(bos);
+
+            FileReader reader = new FileReader(full_path);
+            String raw = reader.readAllAsString();
+            byte[] data = raw.getBytes(StandardCharsets.UTF_8);
+
+            gzip.write(data);
+            gzip.close();
+
+            byte[] compressed = bos.toByteArray();
+
+            var is_success = db.execute("""
+                    INSERT INTO Versions (file_id, parent_id, tag, is_delta, compressed, created_at)
+                    VALUES ( ?, NULL, 'First version', FALSE, ?, datetime('now') )
+                    """,
+                    id, compressed
+            );
+
+            if (!is_success) ErrorDialog.showException("Can not insert a version.");
+
+        } catch (SQLException | IOException e) {
+            var is_dev_mode = Boolean.parseBoolean(System.getProperty("dev"));
+
+            if (is_dev_mode) {
+                ErrorDialog.showDevException(e, "cannot execute database operations");
+            } else {
+                ErrorDialog.showException("cannot execute database operations");
+            }
+        }
+
+        Stage new_stage = new Stage();
+
+        FXMLLoader loader = new FXMLLoader(GitextApp.class.getResource("main-panel.fxml"));
+
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+        } catch (IOException e) {
+            ErrorDialog.showException("Can not open menu.");
+            return;
+        }
+
+        if (id == -1) return;
+
+        final Path path = Path.of(full_path);
+        ((MainPanelController) loader.getController()).onReady(path, id, new_stage);
+        new_stage.setTitle("File: %s".formatted(path.getFileName()));
+        new_stage.setScene(scene);
+        new_stage.show();
+
+        stage.close();
+
+    }
+
+    @FXML
+    private void onSettingPressed() {
+        FXMLLoader loader = new FXMLLoader(GitextApp.class.getResource("setting-view.fxml"));
+
+        try {
+            var scene = new Scene(loader.load());
+            Stage new_stage = new Stage();
+            new_stage.setTitle("Setting");
+            new_stage.setScene(scene);
+            new_stage.show();
+        } catch (IOException e) {
+            ErrorDialog.showException("Can not open menu.");
+        }
+
+    }
 }
