1059aec760be38535fcc0753482343d9eb593a6d
LadsForTea <88262542+Absurdnoobman@users.noreply.github.com>
2026-01-01 17:19:48 +0700

    - add Open First Menu in Menu Bar. - add `SettingManager.java` - using store instead of db.execute()

--- src/main/java/sut/project/oop/gitextfx/controllers/SettingController.java
+++ src/main/java/sut/project/oop/gitextfx/controllers/SettingController.java
@@ -1,4 +1,13 @@
 package sut.project.oop.gitextfx.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+
 public class SettingController {
+    @FXML
+    public CheckBox deleteFileCheckBox;
+
+    private void onReady() {
+        
+    }
 }
